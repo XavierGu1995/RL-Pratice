7f3d31e111d337480c7ff9889da22ad19ecd4eb8
Xavier Gu <XavierGu1995@gmail.com>
2019-07-11 18:05:31 +0800

    Add files via upload

--- PolicyGradient.py
+++ PolicyGradient.py
@@ -2,15 +2,12 @@ import numpy as np
 import torch
 import torch.nn as nn
 import gym
-import time
 
-LR = 0.001
-GAMMA = 0.9
-TAU = 0.01
-BATCH_SIZE = 32
-MAX_EPISODES = 200
-MAX_EP_STEPS = 200
+LR = 0.01
+GAMMA = 0.95
 DISPLAY_REWARD_THRESHOLD = 400
+
+ENV_NAME = 'CartPole-v0'
 RENDER = False  # rendering wastes time
 
 
@@ -26,8 +23,8 @@ class Net(nn.Module):
     def forward(self, x):
         x = self.fc1(x)
         x = torch.tanh(x)
-        actions = self.fc2(x)
-        actions_prob = torch.softmax(actions, dim=0)
+        x = self.fc2(x)
+        actions_prob = torch.softmax(x, dim=1)
         return actions_prob
 
 
@@ -36,68 +33,81 @@ class PolicyGradient(object):
     def __init__(self, s_dim, a_dim):
         self.s_dim = s_dim
         self.a_dim = a_dim
-        self.net = Net()
+        self.net = Net(s_dim, a_dim)
         self.optimizer = torch.optim.Adam(self.net.parameters(), lr=LR)
 
-    def choose_action(self, x):
-        x = torch.unsqueeze(torch.tensor(x, dtype=torch.float32), 0)
-        prob_weights = self.net.forward(x)
-        action = np.random.choice(range(prob_weights.shape[1]),
-                                  p=prob_weights.ravel())
+        self.ep_obs, self.ep_as, self.ep_rs = [], [], []
+
+    def choose_action(self, s):
+        s = torch.unsqueeze(torch.tensor(s, dtype=torch.float32), 0)
+        prob_weights = self.net.forward(s).detach()
+        prob_weights.requires_grad = False
+        a = np.random.choice(range(prob_weights.shape[1]),
+                             p=prob_weights.squeeze().numpy())
+        return a
 
     def store_transition(self, s, a, r):
-
+        self.ep_obs.append(s)
+        self.ep_as.append(a)
+        self.ep_rs.append(r)
 
     def learn(self):
+        r = 0
+        for t in range(len(self.ep_rs)):
+            r = r * GAMMA + self.ep_rs[t]
 
+        s = torch.tensor(self.ep_obs, dtype=torch.float32)
+        a = torch.tensor(self.ep_as, dtype=torch.long)
+
+        prob_weights = self.net(s).gather(1, a.reshape(len(self.ep_rs), 1))
+        log_prob_weights = torch.log(prob_weights)
+        loss = -(r-200) * torch.mean(log_prob_weights)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        self.ep_obs, self.ep_as, self.ep_rs = [], [], []
 
 
 if __name__ == '__main__':
 
-    env = gym.make('Pendulum-v0')
-    env = env.unwrapped
+    env = gym.make(ENV_NAME)
     env.seed(1)
-    s_dim = env.observation_space.shape[0]
-    a_dim = env.action_space.shape[0]
-    a_bound = env.action_space.high
+    env = env.unwrapped
 
-    RL = PolicyGradient(s_dim, a_dim)
+    state_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.n
 
-    var = 3  # control exploration
-    t1 = time.time()
-    for i in range(MAX_EPISODES):
-        s = env.reset()
-        ep_reward = 0
-        for j in range(MAX_EP_STEPS):
+    RL = PolicyGradient(state_dim, action_dim)
+
+    for i_episode in range(3000):
+        state = env.reset()
+
+        while True:
             if RENDER:
                 env.render()
 
-            # Add exploration noise
-            a = RL.choose_action(s)
-            # add randomness to action selection for exploration
-            a = np.clip(np.random.normal(a, var), -2, 2)
-            s_, r, done, info = env.step(a)
+            action = RL.choose_action(state)
 
-            RL.store_transition(s, a, r / 10)
+            state_, reward, done, info = env.step(action)
 
-            if ddpg.pointer > MEMORY_CAPACITY:
-                var *= .9995  # decay the action randomness
-                ddpg.learn()
+            RL.store_transition(state, action, reward)
 
-            s = s_
-            ep_reward += r
-            if j == MAX_EP_STEPS - 1:
-                print(
-                    'Episode:',
-                    i,
-                    ' Reward: %i' %
-                    int(ep_reward),
-                    'Explore: %.2f' %
-                    var,
-                )
-                if ep_reward > -300:
+            if done:
+                ep_rs_sum = sum(RL.ep_rs)
+
+                if 'running_reward' not in globals():
+                    running_reward = ep_rs_sum
+                else:
+                    running_reward = 0.99 * running_reward + ep_rs_sum * 0.01
+
+                if running_reward > DISPLAY_REWARD_THRESHOLD:
                     RENDER = True
+                print("episode:", i_episode, "  reward:", int(running_reward))
+
+                RL.learn()
+
                 break
-    print('Running time: ', time.time() - t1)
 
-
+            state = state_
